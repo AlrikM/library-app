5b48f206a9404afdd6c311fcced5135edb34ce1d
AlrikM <alrbri0804@skola.goteborg.se>
2025-12-17 16:10:40 +0100

    added new tests and disabled finished tests

---
+++ backend/src/test/java/se/yrgo/libraryapp/validators/RealNameTest.java
@@ -0,0 +1,8 @@
+package se.yrgo.libraryapp.validators;
+
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+public class RealNameTest {
+
+        }
--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,18 +1,19 @@
 package se.yrgo.libraryapp.validators;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
 public class UsernameTest {
+    @Disabled
     @ParameterizedTest
     @ValueSource(strings = {"Bosse_man@coolStuff", "@-_-."})
     void correctUsername(String goodName) {
         assertTrue(Username.validate(goodName));
     }
-
+    @Disabled
     @ParameterizedTest
     @ValueSource(strings = {"Name with space", "tre", "*?!${"})
     @EmptySource
