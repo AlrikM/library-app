b3cbf016a656d0e6c265636cc4bd95a9189b5e7a
AlrikM <alrbri0804@skola.goteborg.se>
2025-12-17 17:07:53 +0100

    added bad words and updated tests

--- backend/src/test/java/se/yrgo/libraryapp/validators/RealNameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/RealNameTest.java
@@ -1,5 +1,8 @@
 package se.yrgo.libraryapp.validators;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
--- backend/src/test/java/se/yrgo/libraryapp/validators/UtilsTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UtilsTest.java
@@ -1,19 +1,20 @@
 package se.yrgo.libraryapp.validators;
 
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.*;
 import org.junit.jupiter.params.provider.*;
 
 public class UtilsTest {
-    @Disabled
+
     @ParameterizedTest
     @CsvSource ({"...bRa4v3e mAn came TO DIE@!, brave man came to die", "heLLo -_123therE!!!, hello there"})
     void onlyLetAndSpaTest(String sentence, String expected){
         assertEquals(expected, Utils.onlyLettersAndWhitespace(sentence));
-        }
-    @Disabled
+    }
+
     @ParameterizedTest
-    @CsvSource ({"6055 i5 h3r3, boss is here", "f4110u7, fallout"})
+    @CsvSource ({"6055 i5 2h3r39, boss is here", "f4110u72, fallout"})
     void unLeetTest(String sentence, String expected){
         assertEquals(expected, Utils.cleanAndUnLeet(sentence));
     }
